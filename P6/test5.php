b7afd6357c2bfde12732a1e6ac984dfa15590637
fauzianfajare-spec <fauzianfajare98@gmail.com>
2025-10-18 20:32:29 +0800

    Add files via upload

---
+++ P6/test5.php
@@ -0,0 +1,9 @@
+<?php
+$teks = "Hello World!";
+$sebuah_bilangan = 4;
+$bilanganYanglain = 8.567;
+$teks2 = $teks;
+echo "<br />" . $sebuah_bilangan;
+echo "<br />Isi dari variabel \$bilanganYanglain : " . $bilanganYanglain;
+echo "<br />Isi dari variabel \$teks2 adalah : " . $teks2;
+?>
---
+++ P6/test7.php
@@ -0,0 +1,18 @@
+<?php
+echo "Hello World 1! </br>"; // perintah ini akan mencetak Hello World 1!
+?>
+
+<?php
+/*
+Berikut ini adalah perintah unutk menampilkan teks Hello World pada
+browser
+*/
+echo "Hello Word 2! </br>";
+?>
+
+<?php
+//Berikut ini adalah perintah
+// Untuk menampilkan teks Hello Word
+// pada browser
+echo "Hello Word 3!";
+?>
