cb91eff36416ad01441a72a8a6386681502f7444
fauzianfajare-spec <fauzianfajare98@gmail.com>
2025-11-12 15:47:25 +0700

    Delete P11/logika_huruf.php

--- P11/logika_huruf.php
+++
@@ -1,9 +0,0 @@
-<?php
-$huruf = "A";
-if (($huruf == "A") || ($huruf == "I") || ($huruf == "U") || ($huruf == "E") || ($huruf == "O")) {
-    echo "Termasuk huruf vokal";
-}
-else {
-    echo "Tidak termasuk huruf vokal";
-}
-?>
